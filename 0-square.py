1249353f82cdb7fc344c64563f0aab60209807d6
Dabztechwyta <healervic17@gmail.com>
2023-12-19 15:37:43 -0800

    Task - Classes and  objects

---
+++ 0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+   A class that defines a square
+'''
+
+class Square:
+    '''
+       An empty class called Square
+    '''
+
+    pass
---
+++ 1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+  A new class Square
+'''
+
+class Square:
+    '''
+      A class that defines a square's size
+    '''
+
+    def __init__(self, size):
+        '''
+        initializes a square
+        
+        Attribute:
+           size: size of square
+        '''
+        self.__size = size
